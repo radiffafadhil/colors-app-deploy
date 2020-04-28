b90372bbe9e8b9fd39c45a66e0923908cc038c7c
ACER <mhdiqbaltan@gmail.com>
2020-04-28 21:20:04 +0700

    initialize commit

---
+++ src/Palette.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import './Palette.css';
+import Navbar from './Navbar';
+
+class Palette extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { level: 500 };
+		this.changeLevel = this.changeLevel.bind(this);
+	}
+
+	changeLevel(level) {
+		this.setState({ level });
+	}
+
+	render() {
+		const { level } = this.state;
+		const colorBoxes = this.props.palette.colors[level].map((color) => (
+			<ColorBox background={color.hex} name={color.name} />
+		));
+		return (
+			<div className="Palette">
+				<Navbar level={level} changeLevel={this.changeLevel} />
+				<div className="Palette-colors">{colorBoxes}</div>
+				{/* footer */}
+			</div>
+		);
+	}
+}
+
+export default Palette;
